cd25a22bd6e9a48efdd3764fbea414f66585a6b3
Kaviraghul <vettrithamil@gmail.com>
2023-01-24 11:04:06 +0530

    added routes and string manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pace_assignment/presentation/home/home_view.dart';
+import 'package:pace_assignment/presentation/resources/strings_manager.dart';
+
+class Routes {
+  static const String mainRoute = "/";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: const Text(AppString.noRouteFound)),
+              body: const Center(child: Text(AppString.noRouteFound)),
+            ));
+  }
+}
---
+++ lib/presentation/resources/strings_manager.dart
@@ -0,0 +1,3 @@
+class AppString {
+  static const String noRouteFound = "no route found";
+}
