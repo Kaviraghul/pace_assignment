3e1985d6cd0e5e6b1c3486bdcdfaf5396931c57f
Kaviraghul <vettrithamil@gmail.com>
2023-01-25 12:23:42 +0530

    added local cache to lessen the api calls

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_field
+
 import 'package:dartz/dartz.dart';
+import 'package:pace_assignment/data/data_source/local_data_source.dart';
 import 'package:pace_assignment/data/data_source/remote_data_source.dart';
 import 'package:pace_assignment/data/mappers/mappers.dart';
 import 'package:pace_assignment/data/network/error_handler.dart';
@@ -9,28 +12,36 @@ import 'package:pace_assignment/domain/repository.dart';
 
 class RepositoryImpl extends Repository {
   final RemoteDataSource _remoteDataSource;
+  final LocalDataStorage _localDataSource;
   final NetworkInfo _networkInfo;
 
-  RepositoryImpl(this._networkInfo, this._remoteDataSource);
+  RepositoryImpl(
+      this._networkInfo, this._remoteDataSource, this._localDataSource);
 
   @override
   Future<Either<Failure, Home>> getNews() async {
-    if (await _networkInfo.isConneected) {
-      try {
-        final response = await _remoteDataSource.getNews();
+    try {
+      final response = await _localDataSource.getNews();
+      return Right(response.toDomain());
+    } catch (cacheError) {
+      if (await _networkInfo.isConneected) {
+        try {
+          final response = await _remoteDataSource.getNews();
 
-        if (response.status == ApiInternalStatus.OK) {
-          return Right(response.toDomain());
-        } else {
-          return Left(Failure(response.status ?? ApiInternalStatus.ERROR,
-              response.status ?? ResponseMessage.DEFAULT));
+          if (response.status == ApiInternalStatus.OK) {
+            _localDataSource.saveNewsToCache(response);
+            return Right(response.toDomain());
+          } else {
+            return Left(Failure(response.status ?? ApiInternalStatus.ERROR,
+                response.status ?? ResponseMessage.DEFAULT));
+          }
+        } catch (error) {
+          return (Left(ErrorHandler.handle(error).failure));
         }
-      } catch (error) {
-        return (Left(ErrorHandler.handle(error).failure));
+      } else {
+        // return default response
+        return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
       }
-    } else {
-      // return default response
-      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
