d9bbb6613dee7b955320e6bbf9d5e72e631971f3
Kaviraghul <vettrithamil@gmail.com>
2023-01-25 12:31:38 +0530

    new article screen setup completed

---
+++ lib/presentation/news_article/news_article_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsArticleView extends StatefulWidget {
+  const NewsArticleView({Key? key}) : super(key: key);
+
+  @override
+  _NewsArticleViewState createState() => _NewsArticleViewState();
+}
+
+class _NewsArticleViewState extends State<NewsArticleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
